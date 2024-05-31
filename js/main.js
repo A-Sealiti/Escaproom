609b4c947ce86c800469f28f508c258b2f0c0965
A-Sealiti <143789798+A-Sealiti@users.noreply.github.com>
2024-05-31 21:59:15 +0200

    project overgezet

--- js/main.js
+++ js/main.js
@@ -1,150 +1,84 @@
-let randomNumber1 = document.querySelector('.randomNumber1')
-let randomNumber2 = document.querySelector('.randomNumber2')
-let randomNumber3 = document.querySelector('.randomNumber3')
-let randomNumber4 = document.querySelector('.randomNumber4')
-let randomNumber5 = document.querySelector('.randomNumber5')
-let randomNumber6 = document.querySelector('.randomNumber6')
-let randomNumber7 = document.querySelector('.randomNumber7')
-let randomNumber8 = document.querySelector('.randomNumber8')
-let randomNumber9 = document.querySelector('.randomNumber9')
-let randomNumber10 = document.querySelector('.randomNumber10')
-let geus = 0;
-let password = '';
-var delayInMilliseconds = 1000;
-const btn = document.querySelector('.button');
-btn.addEventListener('click', myFunction);
-let tryouts = 0
-function myFunction() {
-    randomNumber1.textContent = Math.floor(Math.random() * 9999) + 1 
-    randomNumber2.textContent = Math.floor(Math.random() * 9999) + 1;
-    randomNumber3.textContent = Math.floor(Math.random() * 9999) + 1;
-    randomNumber4.textContent = Math.floor(Math.random() * 9999) + 1;
-    randomNumber5.textContent = Math.floor(Math.random() * 9999) + 1;
-    randomNumber6.textContent = Math.floor(Math.random() * 9999) + 1;
-    randomNumber7.textContent = Math.floor(Math.random() * 9999) + 1;
-    randomNumber8.textContent = Math.floor(Math.random() * 9999) + 1;
-    randomNumber9.textContent = Math.floor(Math.random() * 9999) + 1;
-    randomNumber10.textContent = Math.floor(Math.random() * 9999) + 1;
-    console.log(randomNumber1)
-    console.log(randomNumber2)
-    console.log(randomNumber3)
-    console.log(randomNumber4)
-    console.log(randomNumber5)
-    console.log(randomNumber6)
-    console.log(randomNumber7)
-    console.log(randomNumber8)
-    console.log(randomNumber9)
-    console.log(randomNumber10)
-    geus = Math.floor(Math.random() * 10) + 1;
+import {InputHandler} from './input.js'
+import {RoomOne, RoomTwo} from './rooms.js'
+// import {Player} from './player.js'
+// import {Rect} from './rect.js'
+// import {Tools} from './tools.js'
 
 
-    setTimeout(function() {
-        myfunction1()
-      }, delayInMilliseconds);
-    
-}
+window.addEventListener('load', () => {
+    const canvas = document.querySelector('.mainCanvas'); // de canvas
+    const ctx = canvas.getContext('2d'); // hier maken we een 2d context
+    canvas.width = 600; // gewoon de grootte enzo
+    canvas.height = 600;
+    ctx.imageSmoothingEnabled = false;
 
-function myfunction1() {
-    if (geus = 1) {
-        while (password !== randomNumber1) {
-            password = prompt('gokjuist1')
-            if(tryouts = 3){
-                password == randomNumber1
+
+    class Game {
+        constructor(width, height) {
+            this.rooms = [new RoomOne(this), new RoomTwo(this)];
+            this.currentRoom = 0;
+            this.ctx = ctx; // contextmanager
+            this.width = width; 
+            this.height = height;
+            this.desiredFPS = 30; // me skere fps handling systeem
+            this.input = new InputHandler();    
+            this.loop = this.mainloop.bind(this); // Eerlijk, geen idee waarom het werkt, maar het werkt wel dus ja  
+            canvas.addEventListener('mousedown', (e) => {this.rooms[this.currentRoom].handleMouseDown(e)});
+            canvas.addEventListener('mouseup', (e) => {this.rooms[this.currentRoom].handleMouseUp(e)});
+            // die goofy ah fps controle systeem
+            this.now = Date.now();
+            this.then = Date.now();
+            this.updateInterval = 1000 / this.desiredFPS;
+            this.elapsed = 0;
+            this.mouseX = 0;
+            this.mouseY = 0;
+            canvas.addEventListener('mousemove', (e) => {
+                this.mouseX = e.pageX - e.currentTarget.offsetLeft + canvas.width / 2;
+                this.mouseY = e.pageY - e.currentTarget.offsetTop + canvas.height / 2;
+            })
+        }
+
+        update() {
+            this.rooms[this.currentRoom].update();
+            this.ctx.fillStyle = 'black'
+            this.rooms[this.currentRoom].draw();
+            this.ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
+            // this.debugWalls();
+        }
+
+
+        debugWalls() { // TIJDELIJK
+            this.rooms[this.currentRoom].walls.forEach((w, i) => {
+                this.ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
+                this.ctx.fillRect(w.x, w.y, w.w, w.h);
+                this.ctx.fillStyle = 'black';
+                this.ctx.strokeText(i, w.x+5, w.y+10);
+            });
+
+            const x = this.rooms[this.currentRoom].terminalCollider;
+            this.ctx.fillRect(x.x, x.y, x.w, x.h)
+        }
+
+
+        mainloop() {
+
+            window.requestAnimationFrame(this.loop); // forceer een update
+            
+            this.now = Date.now();
+            this.elapsed = this.now - this.then; // die skere slome fps ding weer
+            
+            if (this.elapsed > this.updateInterval) {
+                this.then = this.now - (this.elapsed % this.updateInterval);
+                this.update(); // update heel de ding na een bepalde tijd. De tijd word door het desiredFPS bepaald\
             }
-            tryouts++
+        }
+
+        nextRoom() {
+            this.currentRoom++;
+            this.input.reset(); // Tijdelijke bug oplossing
         }
     }
-    else if (geus = 2) {
-        while (password !== randomNumber2) {
-            password = prompt('gokjuist2')
-            if(tryouts = 3){
-                password == randomNumber1
-            }
-            tryouts++
 
-        }
-    }
-    else if (geus = 3) {
-        while (password !== randomNumber3) {
-            password = prompt('gokjuist3')
-            if(tryouts = 3){
-                password == randomNumber1
-            }
-            tryouts++
-        }
-    }
-    else if (geus = 4) {
-        while (password !== randomNumber4) {
-            password = prompt('gokjuist4')
-            if(tryouts = 3){
-                password == randomNumber1
-            }
-            tryouts++
-        }
-    }
-    else if (geus = 5) {
-        while (password !== randomNumber5) {
-            password = prompt('gokjuist5')
-            if(tryouts = 3){
-                password == randomNumber1
-            }
-            tryouts++
-        }
-    }
-    else if (geus = 6) {
-        while (password !== randomNumber6) {
-            password = prompt('gokjuist6')
-            if(tryouts = 3){
-                password == randomNumber1
-            }
-            tryouts++
-        }
-    }
-    else if (geus = 7) {
-        while (password !== randomNumber7) {
-            password = prompt('gokjuist7')
-            if(tryouts = 3){
-                password == randomNumber1
-            }
-            tryouts++
-        }
-    }
-    else if (geus = 8) {
-        while (password !== randomNumber8) {
-            password = prompt('gokjuist8')
-            if(tryouts = 3){
-                password == randomNumber1
-            }
-            tryouts++
-        }
-    }
-    else if (geus = 9) {
-        while (password !== randomNumber9) {
-            password = prompt('gokjuist9')
-            if(tryouts = 3){
-                password == randomNumber1
-            }
-            tryouts++
-        }
-    }
-    else if (geus = 10) {
-        while (password !== randomNumber10) {
-            password = prompt('gokjuist10')
-            if(tryouts = 3){
-                password == randomNumber1
-            }
-            tryouts++
-        }
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
+    const game = new Game(canvas.width, canvas.height); // spreekt voor zich
+    game.mainloop();
+});
